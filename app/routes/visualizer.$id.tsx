e32b11250dece7d4be03fa4f090f40057653d16d
teo juhendra <teojuhendra@gmail.com>
2026-02-21 10:23:24 +0700

    host and upload images

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useLocation } from "react-router";
 
 const visualizerId = () => {
-  return <div>visualizer.$id</div>;
+  const location = useLocation();
+  const { initialImage, name } = location.state || {};
+
+  return (
+    <section>
+      <h1>{name || "Unititled Design"}</h1>
+
+      <div className="visualizer">
+        {initialImage && (
+          <div className="image-container">
+            <h2>Source Image</h2>
+            <img src={initialImage} alt="source" />
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default visualizerId;
